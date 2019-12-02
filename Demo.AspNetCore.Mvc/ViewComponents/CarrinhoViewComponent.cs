7e6e1115aa81129e9bfc5302e39a0e74bcaeea68
Bruno Barreto <bruno.barreto@selfpay.com.br>
2019-12-01 21:45:35 -0300

    Testes Tag Helpers e de View Components

---
+++ Demo.AspNetCore.Mvc/ViewComponents/CarrinhoViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Demo.AspNetCore.Mvc.ViewComponents
+{
+    [ViewComponent(Name = "Carrinho")]
+    public class CarrinhoViewComponent : ViewComponent
+    {
+        public int itensCarrinho { get; set; }
+
+        public CarrinhoViewComponent()
+        {
+            itensCarrinho = 3;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View(itensCarrinho);
+        }
+    }
+}
