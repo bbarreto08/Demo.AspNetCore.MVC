123af223fd430dc99be1850c3c89f06e9f6446ee
Bruno Barreto <bruno.barreto@selfpay.com.br>
2019-12-03 20:52:23 -0300

    Testando o scafold

---
+++ Demo.AspNetCore.Mvc/Data/DemoAspNetCoreMvcContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Demo.AspNetCore.Mvc.Models
+{
+    public class DemoAspNetCoreMvcContext : DbContext
+    {
+        public DemoAspNetCoreMvcContext (DbContextOptions<DemoAspNetCoreMvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Demo.AspNetCore.Mvc.Models.Filme> Filme { get; set; }
+    }
+}
